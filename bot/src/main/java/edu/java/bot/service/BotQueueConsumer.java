d7f6dea4979a01056db0539140f13fe874a67f99
aleckbb <belozerov200@gmail.com>
2024-04-16 16:30:10 +0300

    hw8

--- bot/src/main/java/edu/java/bot/service/BotQueueConsumer.java
+++ bot/src/main/java/edu/java/bot/service/BotQueueConsumer.java
@@ -7,6 +7,7 @@ import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,10 +16,12 @@ import org.springframework.stereotype.Service;
 public class BotQueueConsumer {
     @Autowired
     public UpdateSender updateSender;
+    @Autowired
+    public KafkaTemplate<String, LinkUpdate> kafkaTemplate;
 
     @RetryableTopic(attempts = "1", kafkaTemplate = "kafkaTemplate")
     @KafkaListener(
-        topics = "${app.kafka.topic-name}",
+        topics = "update-messages-topic",
         groupId = "group1",
         containerFactory = "kafkaListenerContainerFactory"
     )
@@ -30,5 +33,6 @@ public class BotQueueConsumer {
     @DltHandler
     public void handleUpdate(LinkUpdate linkUpdate) {
         log.info("message received to dlt");
+        kafkaTemplate.send("dlt-topic", linkUpdate);
     }
 }
